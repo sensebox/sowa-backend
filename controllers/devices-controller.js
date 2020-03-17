095923e7851168c4cb6c0d1b378b6b18fec6cee3
Benjamin Karic <b_kari02@uni-muenster.de>
2020-03-17 03:42:08 +0100

    create routes for device requests, cleanup and debugging

--- controllers/devices-controller.js
+++ controllers/devices-controller.js
@@ -2,6 +2,8 @@ const SparqlClient = require('sparql-client-2');
 const SPARQL = SparqlClient.SPARQL;
 const endpoint = 'http://localhost:3030/senph/sparql';
 const updatepoint = 'http://localhost:3030/senph/update';
+const history_endpoint = 'http://localhost:3030/senph-history/sparql';
+const history_updatepoint = 'http://localhost:3030/senph-history/update';
 // const unitpoint = 'http://localhost:3030/uo/sparql';
 
 
@@ -25,6 +27,18 @@ const client = new SparqlClient(endpoint, {
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     xsd: 'http://www.w3.org/2001/XMLSchema#'
 
+  })
+
+const historyClient = new SparqlClient(history_endpoint, {
+  updateEndpoint: history_updatepoint
+})
+  .register({
+    owl: 'http://www.w3.org/2002/07/owl#',
+    rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+    s: 'http://www.opensensemap.org/SENPH#',
+    uo: 'http://purl.obolibrary.org/obo/',
+    rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    xsd: 'http://www.w3.org/2001/XMLSchema#'
 
   })
 
@@ -38,29 +52,54 @@ module.exports.getDevices = function () {
     .query(SPARQL`
                      SELECT ?label ?device
                      WHERE {
-                       ?device rdf:type s:device.
-                       OPTIONAL{
-                       ?device rdfs:label ?label}
+                        ?device rdf:type s:device.
+                        OPTIONAL{
+                          ?device rdfs:label ?label
+                        }
                     }`)
     .execute({ format: { resource: 'device' } })
     .then(res => res.results.bindings)
     .catch(function (error) {
+      console.dir(arguments, { depth: null })
       console.log("Oh no, error!")
+      console.log(error)
     });
 }
 
+//get history of a device
+module.exports.getDeviceHistory = function (iri) {
+  var senphurl = 'http://www.opensensemap.org/SENPH#';
+  var bindingsText = `
+  SELECT ?device
+                     WHERE {
+                       ?device rdf:type s:device.
+                       FILTER(regex(str(?device), ?iri, "i" ))
+                     }`;
+  return historyClient
+    .query(bindingsText)
+    .bind('iri', senphurl + iri)
+    .execute()
+    .then(res => {
+      console.log(res.results.bindings)
+      return res.results.bindings
+    })
+    .catch(function (error) {
+      console.dir(arguments, { depth: null })
+      console.log("Oh no, error!")
+      console.log(error)
+    });
+}
+
+
 //get a single device identified by its iri @returns the device's labels, descriptions, website, image, contact and compatible sensors
 module.exports.getDevice = function (iri) {
+  var senphurl = 'http://www.opensensemap.org/SENPH#';
+
   return client
     .query(SPARQL`
-    Select Distinct ?iri ?label ?description ?website ?image ?contact ?sensors ?sensorsLabel 
-                     WHERE {   
-  						{	
-                            ${{ s: iri }}  rdfs:label ?name.
-                          ?iri ?rdf ?name
-                        }
-                        UNION 
-                        {   
+    Select Distinct ?label ?description ?website ?image ?contact ?sensor ?sensorLabel 
+                     WHERE {
+                        {
                             ${{ s: iri }} rdfs:label ?label
                         }
                         UNION 
@@ -81,54 +120,123 @@ module.exports.getDevice = function (iri) {
                         }   
                         UNION
                         {
-                            ${{ s: iri }} s:hasSensor ?sensors.
-                            ?sensors rdfs:label ?sensorsLabel.
+                            ${{ s: iri }} s:hasSensor ?sensor.
                         }   
                      }
-                Group BY ?iri ?label ?description ?website ?image ?contact ?sensors ?sensorsLabel 
-                ORDER BY ?iri ?sensors
+                Group BY ?label ?description ?website ?image ?contact ?sensor ?sensorLabel 
+                ORDER BY ?sensor
           `)
     .execute()
-    .then(res => res.results.bindings)
-    .catch(function (error) {
-      console.log("Oh no, error!")
-    });
-}
-
-
-//update/add a new device @inputs required: label + language, description + language; optional: website, image, contact, compatible sensor
-module.exports.updateDevice = function (device) {
-  var senphurl = 'http://www.opensensemap.org/SENPH#';
-  console.log(device);
-  var bindingsText = 'INSERT DATA {' +
-    '?deviceURI rdf:type     s:device.' +
-    '?deviceURI rdfs:label   ?deviceLabel. ' +
-    '?deviceURI rdfs:comment ?desc.' +
-    (device.website ? '?deviceURI s:website ?website.' : '') +
-    (device.image ? '?deviceURI s:image ?image.' : '') +
-    (device.contact ? '?deviceURI s:hasContact ?contact.' : '') +
-    (device.sensor ? '?deviceURI s:hasSensor ?sensor.' : '') +
-    '}';
-  return client
-    .query(bindingsText)
-    .bind({
-      deviceURI: { value: senphurl + device.uri, type: 'uri' },
-      deviceLabel: { value: device.name.label, lang: device.name.lang },
-      desc: { value: device.description.comment, lang: device.description.lang },
-      website: { value: device.website, type: 'string' },
-      image: { value: device.image, type: 'string' },
-      contact: { value: device.contact, type: 'string' },
-      sensor: { value: senphurl + device.sensor, type: 'uri' }
+    .then(res => {
+      console.log(res);
+      res.results.bindings.push({
+        'iri':
+        {
+          type: 'uri',
+          value: senphurl + iri
+        }
+      })
+      console.log(res.results.bindings);
+      return res.results.bindings;
     })
-    .execute()
-    .then(Promise.resolve(console.log("everthing ok")))
     .catch(function (error) {
-      console.log(error);
+      console.dir(arguments, { depth: null })
+      console.log("Oh no, error!")
+      console.log(error)
     });
 }
 
 
-//get a single device identified by its iri @returns the device's labels, descriptions, website, image, contact and compatible sensors
+
+//get a single historic device version identified by its iri @returns the device's labels, descriptions, website, image, contact and compatible sensors
+module.exports.getHistoricDevice = function (iri) {
+  var senphurl = 'http://www.opensensemap.org/SENPH#';
+
+  return historyClient
+    .query(SPARQL`
+    Select Distinct ?label ?description ?website ?image ?contact ?sensor ?sensorLabel 
+                     WHERE {
+                        {
+                            ${{ s: iri }} rdfs:label ?label
+                        }
+                        UNION 
+                        {   
+                            ${{ s: iri }} rdfs:comment ?description
+                        }
+                        UNION
+                        {
+                            ${{ s: iri }} s:website ?website.
+                        }  
+                        UNION
+                        {
+                            ${{ s: iri }} s:image ?image.
+                        } 
+                        UNION
+                        {
+                            ${{ s: iri }} s:hasContact ?contact.
+                        }   
+                        UNION
+                        {
+                            ${{ s: iri }} s:hasSensor ?sensor.
+                        }   
+                     }
+                Group BY ?label ?description ?website ?image ?contact ?sensor ?sensorLabel 
+                ORDER BY ?sensor
+          `)
+    .execute()
+    .then(res => {
+      console.log(res);
+      res.results.bindings.push({
+        'iri':
+        {
+          type: 'uri',
+          value: senphurl + iri
+        }
+      })
+      console.log(res.results.bindings);
+      return res.results.bindings;
+    })
+    .catch(function (error) {
+      console.dir(arguments, { depth: null })
+      console.log("Oh no, error!")
+      console.log(error)
+    });
+}
+
+
+// //update/add a new device @inputs required: label + language, description + language; optional: website, image, contact, compatible sensor
+// module.exports.updateDevice = function (device) {
+//   var senphurl = 'http://www.opensensemap.org/SENPH#';
+//   console.log(device);
+//   var bindingsText = 'INSERT DATA {' +
+//     '?deviceURI rdf:type     s:device.' +
+//     '?deviceURI rdfs:label   ?deviceLabel. ' +
+//     '?deviceURI rdfs:comment ?desc.' +
+//     (device.website ? '?deviceURI s:website ?website.' : '') +
+//     (device.image ? '?deviceURI s:image ?image.' : '') +
+//     (device.contact ? '?deviceURI s:hasContact ?contact.' : '') +
+//     (device.sensor ? '?deviceURI s:hasSensor ?sensor.' : '') +
+//     '}';
+//   return client
+//     .query(bindingsText)
+//     .bind({
+//       deviceURI: { value: senphurl + device.uri, type: 'uri' },
+//       deviceLabel: { value: device.name.label, lang: device.name.lang },
+//       desc: { value: device.description.comment, lang: device.description.lang },
+//       website: { value: device.website, type: 'string' },
+//       image: { value: device.image, type: 'string' },
+//       contact: { value: device.contact, type: 'string' },
+//       sensor: { value: senphurl + device.sensor, type: 'uri' }
+//     })
+//     .execute()
+//     .then(Promise.resolve(console.log("everthing ok")))
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// }
+
+
+//edit a device
 module.exports.editDevice = function (device) {
   var senphurl = 'http://www.opensensemap.org/SENPH#';
   console.log(device);
@@ -137,15 +245,19 @@ module.exports.editDevice = function (device) {
   var bindingsText = 'DELETE {?a ?b ?c}' +
     'INSERT {' +
     '?deviceURI rdf:type     s:device.' +
-    '?deviceURI rdfs:label   ?deviceLabel. ' +
     '?deviceURI rdfs:comment ?desc.' +
-    (device.website ? '?deviceURI s:website ?website.' : '') +
-    (device.image ? '?deviceURI s:image ?image.' : '') +
-    (device.contact ? '?deviceURI s:hasContact ?contact.' : '');
+    '?deviceURI s:website ?website.' +
+    '?deviceURI s:image ?image.' +
+    '?deviceURI s:hasContact ?contact.';
   // create insert ;line for each sensor 
   device.sensor.forEach(element => {
     var string = '?deviceURI s:hasSensor s:' + element.sensorUri.slice(34) + '. ';
     bindingsText = bindingsText.concat(string)
+  });
+  device.label.forEach(element => {
+    bindingsText = bindingsText.concat(
+      '?deviceURI rdfs:label ' + JSON.stringify(element.value) + '@' + element.lang + '. '
+    );
   });
   // add WHERE statement 
   bindingsText = bindingsText.concat('} WHERE {?a ?b ?c. FILTER (?a = ?deviceURI || ?c = ?deviceURI)}');
@@ -157,56 +269,136 @@ module.exports.editDevice = function (device) {
     .bind({
       deviceURI: { value: senphurl + device.uri, type: 'uri' },
       // +++ FIXME +++ language hardcoded, make it dynamic
-      deviceLabel: { value: device.name, lang: "en" },
-      // +++ FIXME +++ language hardcoded, make it dynamic
       desc: { value: device.description, lang: "en" },
-      website: { value: device.website, type: 'string' },
-      image: { value: device.image, type: 'string' },
-      contact: { value: device.contact, type: 'string' },
+      website: device.website,
+      image: device.image,
+      contact: device.contact,
     })
     .execute()
-    .then(Promise.resolve(console.log("evertyhing ok")))
-    .catch(function (error) {
-      console.log(error);
-    });
-}    
-//get a single device identified by its iri @returns the device's labels, descriptions, website, image, contact and compatible sensors
-module.exports.addDevice = function (device) {
-  var senphurl = 'http://www.opensensemap.org/SENPH#';
+}
+
+//create new version of a device in history db 
+module.exports.createHistoryDevice = function (device) {
+  device['dateTime'] = Date.now();
   console.log(device);
 
+  var senphurl = 'http://www.opensensemap.org/SENPH#';
+
   // create SPARQL Query: 
-    'INSERT {' +
+  var bindingsText = 'INSERT DATA {' +
     '?deviceURI rdf:type     s:device.' +
-    '?deviceURI rdfs:label   ?deviceLabel. ' +
     '?deviceURI rdfs:comment ?desc.' +
-    (device.website ? '?deviceURI s:website ?website.' : '') +
-    (device.image ? '?deviceURI s:image ?image.' : '') +
-    (device.contact ? '?deviceURI s:hasContact ?contact.' : '');
+    '?deviceURI s:website ?website.' +
+    '?deviceURI s:image ?image.' +
+    '?deviceURI s:hasContact ?contact.';
   // create insert ;line for each sensor 
   device.sensor.forEach(element => {
     var string = '?deviceURI s:hasSensor s:' + element.sensorUri.slice(34) + '. ';
     bindingsText = bindingsText.concat(string)
   });
+  device.label.forEach(element => {
+    bindingsText = bindingsText.concat(
+      '?deviceURI rdfs:label ' + JSON.stringify(element.value) + '@' + element.lang + '. '
+    );
+  });
   // add WHERE statement 
   bindingsText = bindingsText.concat('}');
   console.log(bindingsText);
+
+  return historyClient
+    .query(bindingsText)
+    // bind values to variable names
+    .bind({
+      deviceURI: { value: senphurl + device.uri + '_' + device.dateTime, type: 'uri' },
+      // +++ FIXME +++ language hardcoded, make it dynamic
+      desc: { value: device.description, lang: "en" },
+      website: device.website,
+      image: device.image,
+      contact: device.contact,
+    })
+    .execute()
+}
+
+
+//create new device
+module.exports.createNewDevice = function (device) {
+  console.log(device);
+
+  var senphurl = 'http://www.opensensemap.org/SENPH#';
+
+  // create SPARQL Query: 
+  var bindingsText = 'INSERT DATA {' +
+    '?deviceURI rdf:type     s:device.' +
+    '?deviceURI rdfs:comment ?desc.' +
+    '?deviceURI s:website ?website.' +
+    '?deviceURI s:image ?image.' +
+    '?deviceURI s:hasContact ?contact.';
+  // create insert ;line for each sensor 
+  device.sensor.forEach(element => {
+    var string = '?deviceURI s:hasSensor s:' + element.sensorUri.slice(34) + '. ';
+    bindingsText = bindingsText.concat(string)
+  });
+  device.label.forEach(element => {
+    bindingsText = bindingsText.concat(
+      '?deviceURI rdfs:label ' + JSON.stringify(element.value) + '@' + element.lang + '. '
+    );
+  });
+  // add WHERE statement 
+  bindingsText = bindingsText.concat('}');
+  console.log(bindingsText);
+
   return client
     .query(bindingsText)
     // bind values to variable names
     .bind({
       deviceURI: { value: senphurl + device.uri, type: 'uri' },
       // +++ FIXME +++ language hardcoded, make it dynamic
-      deviceLabel: { value: device.name, lang: "en" },
-      // +++ FIXME +++ language hardcoded, make it dynamic
       desc: { value: device.description, lang: "en" },
-      website: { value: device.website, type: 'string' },
-      image: { value: device.image, type: 'string' },
-      contact: { value: device.contact, type: 'string' },
+      website: device.website,
+      image: device.image,
+      contact: device.contact,
     })
     .execute()
-    .then(Promise.resolve(console.log("evertyhing ok")))
-    .catch(function (error) {
-      console.log(error);
-    });
 }
+
+
+// //get a single device identified by its iri @returns the device's labels, descriptions, website, image, contact and compatible sensors
+// module.exports.addDevice = function (device) {
+//   var senphurl = 'http://www.opensensemap.org/SENPH#';
+//   console.log(device);
+
+//   // create SPARQL Query: 
+//   'INSERT {' +
+//     '?deviceURI rdf:type     s:device.' +
+//     '?deviceURI rdfs:label   ?deviceLabel. ' +
+//     '?deviceURI rdfs:comment ?desc.' +
+//     (device.website ? '?deviceURI s:website ?website.' : '') +
+//     (device.image ? '?deviceURI s:image ?image.' : '') +
+//     (device.contact ? '?deviceURI s:hasContact ?contact.' : '');
+//   // create insert ;line for each sensor 
+//   device.sensor.forEach(element => {
+//     var string = '?deviceURI s:hasSensor s:' + element.sensorUri.slice(34) + '. ';
+//     bindingsText = bindingsText.concat(string)
+//   });
+//   // add WHERE statement 
+//   bindingsText = bindingsText.concat('}');
+//   console.log(bindingsText);
+//   return client
+//     .query(bindingsText)
+//     // bind values to variable names
+//     .bind({
+//       deviceURI: { value: senphurl + device.uri, type: 'uri' },
+//       // +++ FIXME +++ language hardcoded, make it dynamic
+//       deviceLabel: { value: device.name, lang: "en" },
+//       // +++ FIXME +++ language hardcoded, make it dynamic
+//       desc: { value: device.description, lang: "en" },
+//       website: { value: device.website, type: 'string' },
+//       image: { value: device.image, type: 'string' },
+//       contact: { value: device.contact, type: 'string' },
+//     })
+//     .execute()
+//     .then(Promise.resolve(console.log("evertyhing ok")))
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// }
